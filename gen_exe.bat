c4ffb32264ab315d0954076d3b85a94f2521c291
tanukimaru0090 <115340817+tmaru0090@users.noreply.github.com>
2025-01-03 18:26:54 +0900

    Update gen_exe.bat

--- gen_exe.bat
+++ gen_exe.bat
@@ -5,5 +5,5 @@ if not exist "%COREEXT%" (
 	mkdir %COREEXT%
 )
 xcopy "E:\windows\dev-tools\Ruby33-x64\lib\ruby\3.3.0\rubygems\core_ext\" %COREEXT%  /E /I /H /Y
-neri main.rb dxlib_const_variables.rb dxlib_ffi.rb dxlib.rb
+neri %*
 endlocal
